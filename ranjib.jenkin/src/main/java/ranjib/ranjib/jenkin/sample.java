3c3b812a3eb1103229be14b3483fa90f52300bdd
ranjib333 <ranjibsaha333@gmail.com>
2019-08-17 21:18:12 +0530

    new1

---
+++ ranjib.jenkin/src/main/java/ranjib/ranjib/jenkin/sample.java
@@ -0,0 +1,15 @@
+package ranjib.ranjib.jenkin;
+
+import org.testng.annotations.Test;
+
+public class sample {
+	@Test
+	public void demp()
+	{
+		System.out.println(2+3);
+		
+		System.out.println("i love india");
+       System.out.println("hello");
+	}
+
+}
